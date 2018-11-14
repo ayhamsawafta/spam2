ec885cdb04029d745bf4a1823f2de4093fc0e759
ayhamsawafta <43705668+ayhamsawafta@users.noreply.github.com>
2018-11-14 07:16:15 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('message', message => {
 if (message.content === '!start') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 1000000; x++) {
+      for(let x = 0; x < 1000; x++) {
         message.channel.send(`i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits i need credits`)
           .then(m => {
             count++;
